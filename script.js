ce5c62145d43e766052069be45ae5fce25160228
Allan Bartoo Chementer <allanbartoochementer@Allans-MacBook-Pro.local>
2025-10-17 13:03:48 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// function updateTime() {
+//   const timeEl = document.querySelector('[data-testid="test-user-time"]');
+//   timeEl.textContent = Date.now();
+// }
+
+// updateTime(); // initial render
+// setInterval(updateTime, 1000); // optional continuous update
